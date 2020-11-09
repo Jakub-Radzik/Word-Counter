b71a8d1c543db73527038b431c3c6b2a4d8d945a
Jakub Radzik <jradzik4@gmail.com>
2020-11-09 14:18:00 +0100

    GUI start

---
+++ src/GraphicalInterface.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class GraphicalInterface {
+    JFrame frame;
+    JButton showStats;
+    JTextArea text;
+
+    GraphicalInterface(){
+        frame = new JFrame()
+    }
+}
