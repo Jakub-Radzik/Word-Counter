bf369ceac7b5fef7d2cede3ddadd8f5705000916
Jakub Radzik <jradzik4@gmail.com>
2020-11-09 14:39:11 +0100

    Create scrollPane with scalable text area

--- src/GraphicalInterface.java
+++ src/GraphicalInterface.java
@@ -2,10 +2,30 @@ import javax.swing.*;
 
 public class GraphicalInterface {
     JFrame frame;
-    JButton showStats;
     JTextArea text;
+    JScrollPane scrollForText;
+    JLabel statistics;
 
     GraphicalInterface(){
-        frame = new JFrame()
+        frame = new JFrame("Word Counter");
+        text = new JTextArea(10,20);
+        scrollForText = new JScrollPane(text);
+        statistics = new JLabel();
+
+
+        scrollForText.setBounds(10,10,280,300);
+        statistics.setBounds(10,320,280,50);
+
+        statistics.setText("Start typing...");
+        scrollForText.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        scrollForText.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        text.setLineWrap(true);
+
+        frame.add(scrollForText);
+        frame.add(statistics);
+        frame.setLayout(null);
+        frame.setSize(320,500);
+        frame.setResizable(false);
+        frame.setVisible(true);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner s = new Scanner(System.in);
-        System.out.println("Wprowadź swój text: ");
-        WordCounter text = new WordCounter(s.nextLine());
-        text.killerChars();
-        text.wordsArrayCreator();
-        text.wordsCounterDictionaryCreator();
-        text.displayInformation();
+        new GraphicalInterface();
+
+//        Scanner s = new Scanner(System.in);
+//        System.out.println("Wprowadź swój text: ");
+//        WordCounter text = new WordCounter(s.nextLine());
+//        text.killerChars();
+//        text.wordsArrayCreator();
+//        text.wordsCounterDictionaryCreator();
+//        text.displayInformation();
     }
 }
